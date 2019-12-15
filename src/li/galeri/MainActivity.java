66b2301b507a7da2d34c3c006b79b8820e975326
Lia Idealis <liaidealiss@gmail.com>
2019-12-15 19:56:27 +0700

    komit pertama

---
+++ src/li/galeri/MainActivity.java
@@ -0,0 +1,20 @@
+package li.galeri;
+
+//import android.support.v7.app.ActionBarActivity; 
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+ 
+
+public class MainActivity extends Activity implements OnItemClickListener, OnClickListener 
+{
+	 
+
+}
---
+++ src/li/galeri/SingleImage.java
@@ -0,0 +1,22 @@
+package li.galeri;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+ 
+
+public class SingleImage extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.singleimage);
+
+        int imId = this.getIntent().getExtras().getInt("posisi");
+        ImageView iv = (ImageView) findViewById(R.id.singleimage);
+        int image = ImageAdapter.mThumbIds[imId];
+        iv.setImageResource(image);
+
+    }
+}
+
